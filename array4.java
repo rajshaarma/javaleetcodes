aa402a3768fb0f3576108594cc55cee51f4f0388
rajshaarma <rajsharma760076@gmail.com>
2025-03-20 12:19:37 +0530

    Update array4.java

--- array4.java
+++ array4.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int start = 0, end = nums.length-1, fe = -1, le = -1;
-       while(start<=end){
-        int mid = start+(end-start)/2;
-        if (nums[mid] == target){
-            fe = mid;
-            end = mid - 1;
+    public int[] searchRange(int[] nums, int target){
+        int res[]={-1, -1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        res[0] = start;
+        res[1] = end;
+        return res;
+    }
+    public int search(int[] nums, int target, boolean firstorlast){
+        int start = 0, end = nums.length-1, mid, ans = -1;
+        while (start <= end){
+            mid = start+(end-start)/2;
+            if (nums[mid] == target){
+                ans = mid;
+                if(firstorlast){
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            if (nums[mid] < target){
+                start = mid + 1;
+            }  
+            if(nums[mid] > target){
+                end = mid-1;
+            }
         }
-        if (nums[mid] < target){
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
-       }
-        start = 0;
-        end = nums.length-1;
-       while(start<=end){
-        int mid = start+(end-start)/2;
-        if (nums[mid] == target){
-            le = mid;
-            start = mid + 1;
-        } if (nums[mid] < target){
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
-       }
-       int ans[] = {fe, le};
-       return ans;
+        return ans;
     }
 }
